6112ab0b51984ad6dfd82fc4d7eb48f7820fa633
Richard Su <richard.h.su@outlook.com>
2021-05-10 00:24:37 +1000

    ver1.0 - task 1 finished

---
+++ app/src/main/java/com/fit5046/paindiary/MainActivity.java
@@ -0,0 +1,28 @@
+package com.fit5046.paindiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.fit5046.paindiary.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.regButton.setOnClickListener(v -> {
+            startActivity(new Intent(this, Register.class));
+        });
+    }
+}
---
+++ app/src/main/java/com/fit5046/paindiary/Register.java
@@ -0,0 +1,66 @@
+package com.fit5046.paindiary;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.fit5046.paindiary.databinding.ActivityRegisterBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Register extends AppCompatActivity {
+    private static final String TAG = Register.class.getSimpleName();
+    private ActivityRegisterBinding binding;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.regButton.setOnClickListener(v -> {
+            if (binding.passwordBox.getText().toString().equals(binding.confirmBox.getText().toString())){
+                String email = binding.emailBox.getText().toString().trim();
+                String password = binding.passwordBox.getText().toString().trim();
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(Register.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                        updateUI(null);
+                    }
+                });
+            }
+        });
+    }
+
+    public void updateUI(FirebaseUser user){
+        if (user != null) {
+            Toast.makeText(this, "You signed", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+        else {
+            Toast.makeText(this,"Signed failed",Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
